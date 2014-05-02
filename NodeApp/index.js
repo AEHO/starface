565e3dda72cd45982887992582f5b7cecd45f88d
Ciro S. Costa <ciro.costa@usp.br>
2014-05-02 02:08:00 -0300

    login and others

---
+++ NodeApp/index.js
@@ -0,0 +1,46 @@
+var FB_OK = false;
+
+$(function () {
+
+  var $loginBtn = $('#login-btn');
+
+  $.ajaxSetup({ cache: true});
+  $.getScript('//connect.facebook.net/en_UK/all.js', function(){
+    FB.init({
+      appId: '307657009382643',
+    });
+
+    FB.Event.subscribe('auth.authResponseChange', function (response) {
+      console.log(response);
+
+      console.log(response.authResponse);
+
+      if (response.status === 'connected') {
+        var uid = response.authResponse.useID;
+        var accessToken = response.authResponse.accessToken;
+        $loginBtn.hide();
+
+        console.log(accessToken);
+
+        localStorage.setItem('_fbat', accessToken);
+
+        FB_OK = true;
+
+      } else if (response.status === 'not_authorized') {
+
+        // not authorized :(
+
+      } else {
+
+        // an error ocurred
+
+      }
+    });
+
+  });
+
+  $loginBtn.click(function () {
+    FB.login(function () {}, {scope: "read_stream", return_scopes: true});
+  });
+
+});
